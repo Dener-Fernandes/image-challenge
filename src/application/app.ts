70bf66996a608e01212fade29a303a37dfaf09bb
Dener-Fernandes <dener.fernandes.oliveira@gmail.com>
2025-05-18 18:49:32 -0400

    feat: add database config, ImageModel, ImageRepositoryInterface and ImageRepository

--- src/application/app.ts
+++ src/application/app.ts
@@ -2,10 +2,22 @@ import express from "express";
 
 import dotenv from "dotenv";
 import { routes } from "./routes";
+import { connect } from "mongoose";
 
 const app = express();
 
 dotenv.config({ path: "./.env" });
+
+const dataBase = process.env.DATABASE.replace(
+  "<PASSWORD>",
+  process.env.DATABASE_PASSWORD,
+);
+
+connect(dataBase)
+  .then(() => console.log("Connected to database"))
+  .catch((err) => {
+    console.log("Could not connect to database");
+  });
 
 app.use(express.json());
 
